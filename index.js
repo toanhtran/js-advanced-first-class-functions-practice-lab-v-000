117a147234d43dd0725d795e1c19b4c86aedd968
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-01 20:08:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file!
 const logDriverNames = function (drivers) {
-  drivers.forEach(function (drivers){
+  drivers.forEach(function (driver){
     console.log(driver.name);
   });
 };
+
