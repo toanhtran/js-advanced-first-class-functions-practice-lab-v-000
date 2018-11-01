5a4dd9c1cdc277576def57433e6dfc850eb449bb
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-01 20:12:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const logDriversByHometown = function (drivers, hometown) {
 };
 
 const driversByRevenue = function (drivers) {
-  drivers.forEach(function (driver) {
+  return drivers.slice().sort(function (driverOne, driverTwo){
     return driverOne.revenue - driverTwo.revenue;
   });
 };
